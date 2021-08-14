0b7cb7ae1ab0fa7f83f724850a6f3942a194dcf7
wazt <waztfm@gmail.com>
2021-08-14 16:32:37 +0300

    Commit first version of 'glossary to dictionary'

--- index.js
+++ index.js
@@ -9,8 +9,11 @@ let fileVersion;
 // Url goes here
 const webPage = 'https://mlwangbooks.com/glossary-main/';
 
-function getFileNameWithExtension(fileName = initialFileName, extension = fileExtension, version = fileVersion.version) {
-    return `${fileName} - v${version}.${extension}`;
+let dictionaryContent = '';
+const dictionaryContentPlaceholder = '__DICTIONARY_CONTENT__';
+
+function getFileNameWithExtension(fileName, type, skipVersion) {
+    return `${fileName}${!skipVersion ? ` - v${fileVersion.version}` : ''}.${type}`;
 }
 
 function updateFileVersion() {
@@ -19,52 +22,90 @@ function updateFileVersion() {
     });
 }
 
-function saveData(data) {
+function saveData(data, fileName = initialFileName, type = fileExtension, skipVersion = false) {
     if (!fs.existsSync(outputFolder)) {
         fs.mkdirSync(outputFolder);
     }
 
-    const outputPath = `${outputFolder}/${getFileNameWithExtension()}`;
+    const outputPath = `${outputFolder}/${getFileNameWithExtension(fileName, type, skipVersion)}`;
     fs.access(outputPath, fs.constants.F_OK, (err) => {
-        if (err) {
-            fs.writeFile(outputPath, '', (err) => {
-                if (err) throw err;
-                console.log(`${initialFileName} file not found. It will be created.`);
-            });
-        }
-
-        fs.appendFile(outputPath, `${data}`, (err) => {
+        fs.writeFile(outputPath, data, (err) => {
             if (err) throw err;
-            console.log('Data was saved!');
+            console.log(`${fileName} was saved!`);
             updateFileVersion();
         });
     });
 }
 
 function clean(string) {
-    return string.replace(/&nbsp;/g, ' ').trim();
+    return string
+        .replace(/&nbsp;/g, ' ') // replace non-breakable whitespace with a whitespace
+        .replace(/&#8216;|&#8217;|‘|’/g, '\'') // replace magic apostrophes with '
+        .replace(/,\'/g, '\',') // fix commas in listed quoted groups ('this,' to 'this',)
+        .replace(/.\'$/g, '\'.') // fix full stops at the end of definitions ('end.' to 'end'.)
+        .replace(/Ms'.$/g, 'Ms.\'.') // fix broken (^) 'Ms'. to 'Ms.'.
+        .trim(); // remove extra whitespaces
 }
 
-fs.readFile('file-version.json', (err, data) => {
-    if (err) fileVersion = 0;
-    fileVersion = JSON.parse(data);
-    fileVersion.version++;
-});
+function createDictionaryBlock(word, definition) {
+    return `
+        <idx:entry name="default" scriptable="yes" spell="yes">
+            <h5><dt><idx:orth>${word}</idx:orth></dt></h5>
+            <dd>${definition}</dd>
+        </idx:entry>
+        <hr/>
+    `
+}
 
-fetch(webPage)
-    .then(result => result.text())
-    .then(body => {
-        const root = parse(body);
-        // Parsing/filtering goes here, if it's messy, a new function can be created
-        const data = root.querySelector('title').innerText;
-        let definitions = Array.from(root.querySelectorAll('.entry-content.clearfix p')).slice(1, -1);
-        const dictionary = [];
-        definitions.forEach(definition => {
-            const key = clean(definition.querySelector('strong').innerText);
-            const value = clean(definition.innerText).substr(definition.innerText.indexOf(key) + key.length).trim();
-            dictionary.push({
-                key, value
-            });
-        });
-        saveData(JSON.stringify(dictionary));
+function saveContent(dictionaryData) {
+    let content = '';
+    dictionaryData.forEach(data => {
+        content += createDictionaryBlock(data.word, data.definition);
     });
+
+    const built = dictionaryContent.replace(dictionaryContentPlaceholder, content);
+    saveData(built, 'content', 'html', true);
+}
+
+function parsePage() {
+    fetch(webPage)
+        .then(result => result.text())
+        .then(body => {
+            const root = parse(body);
+            // Parsing/filtering goes here, if it's messy, a new function can be created
+            const data = root.querySelector('title').innerText;
+            let definitions = Array.from(root.querySelectorAll('.entry-content.clearfix p')).slice(1, -1);
+            const dictionary = [];
+            definitions.forEach(definition => {
+                const key = clean(definition.querySelector('strong').innerText);
+                const value = clean(definition.innerText).substr(definition.innerText.indexOf(key) + key.length).trim();
+                dictionary.push({
+                    word: key,
+                    definition: value
+                });
+            });
+            saveData(JSON.stringify(dictionary));
+            saveContent(dictionary);
+        });
+}
+
+function getDefaultContent() {
+    fs.readFile('./assets/default_content.html', (err, data) => {
+        if (err) throw err;
+        dictionaryContent = data.toString();
+
+        parsePage();
+    });
+}
+
+function getVersion() {
+    fs.readFile('./file-version.json', (err, data) => {
+        if (err) fileVersion = 0;
+        fileVersion = JSON.parse(data);
+        fileVersion.version++;
+
+        getDefaultContent();
+    });
+}
+
+getVersion();
